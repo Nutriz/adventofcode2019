4c6ca152d7413d2a1eb29a6b6197a242abcfcedb
Nutriz <jerome.gully0@gmail.com>
2019-12-05 13:04:09 +0100

    Day 4 part 2

--- src/main/kotlin/day4.kt
+++ src/main/kotlin/day4.kt
@@ -5,10 +5,14 @@ fun main() {
 
     val (rangeStart, rangeEnd) = inputsDay4.split("-").map(String::toInt)
     val result = (rangeStart..rangeEnd).filter { pwd: Int ->
-        twoConsecutiveNumbersRegex.containsMatchIn(pwd.toString()) && onyIncreaseOrSame.containsMatchIn(pwd.toString())
+        twoConsecutiveNumbersRegex.containsMatchIn(pwd.toString()) && onyIncreaseOrSame.containsMatchIn(pwd.toString()) && onlyTwoConsecutiveNum(pwd)
     }.size
 
     println(result)
+}
+
+fun onlyTwoConsecutiveNum(num: Int) : Boolean {
+    return num.toString().groupBy { it }.any { it.value.size == 2 }
 }
 
 val inputsDay4 = "402328-864247"
